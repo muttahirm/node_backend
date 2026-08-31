00f5ce1f48a4dd6400738d7330f5d6decb57416c
Muttahir Mumtaz <mumtaz.muttahir@gmail.com>
2026-08-31 02:41:39 +0200

    Creates application constants.

---
+++ src/constants/application.constants.ts
@@ -0,0 +1,9 @@
+export const APPLICATION_NAME = 'Node Express MongoDB API';
+
+export const API_PREFIX = '/api/v1';
+
+export const DEFAULT_PAGE = 1;
+
+export const DEFAULT_PAGE_SIZE = 20;
+
+export const MAX_PAGE_SIZE = 100;
